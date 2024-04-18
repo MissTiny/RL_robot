813dbd6ace8d439bb6da2113817b62e3d8b0ce59
Daniel <2421036333@qq.com>
2024-04-18 01:27:23 -0400

    action space def up to middle body

---
+++ Assets/Scripts/AgentController.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class AgentController : Agent
+{
+	[SerializeField] private Transform head;
+	[SerializeField] private Rigidbody rightSmallArmRigidbody;
+	[SerializeField] private Rigidbody leftSmallArmRigidbody;
+	[SerializeField] private Rigidbody middleBodyRigidbody;
+	
+	public float rotationSpeed = 100f;
+	
+	public override void OnEpisodeBegin()
+	{
+		transform.localPosition = new Vector3(0f, 3f, 0f);
+	}
+	
+	public override void CollectObservations(VectorSensor sensor)
+	{
+		sensor.AddObservation(transform.localPosition);
+	}
+	
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+		// Rotation actions for the head
+		float headRotationY = actions.ContinuousActions[0];
+		float headRotationZ = actions.ContinuousActions[1];
+		
+        // Actions for the right small arm
+		float rightTorqueX = actions.ContinuousActions[2];
+		float rightTorqueY = -Mathf.Abs(actions.ContinuousActions[3]);
+
+		// Actions for the left small arm
+		float leftTorqueX = actions.ContinuousActions[4];
+		float leftTorqueY = Mathf.Abs(actions.ContinuousActions[5]);
+		
+		// Action for the middle body's rotation
+		float middleBodyTorqueY = actions.ContinuousActions[6];
+		
+		// Apply the head rotation
+		Vector3 currentHeadRotation = head.localEulerAngles;
+		Vector3 newHeadRotation = new Vector3(
+			currentHeadRotation.x,
+			currentHeadRotation.y + headRotationY * rotationSpeed * Time.deltaTime,
+			ClampAngle(currentHeadRotation.z + headRotationZ * rotationSpeed * Time.deltaTime, -30f, 30f)
+		);
+		head.localEulerAngles = newHeadRotation;
+
+		// Apply the right and left small arms' torque
+		rightSmallArmRigidbody.AddRelativeTorque(new Vector3(rightTorqueX, rightTorqueY, 0) * rotationSpeed);
+		leftSmallArmRigidbody.AddRelativeTorque(new Vector3(leftTorqueX, leftTorqueY, 0) * rotationSpeed);
+		
+		// Apply the middle body's torque
+		middleBodyRigidbody.AddRelativeTorque(Vector3.forward * middleBodyTorqueY * rotationSpeed);
+    }
+	
+	private float ClampAngle(float angle, float min, float max)
+	{
+		angle = NormalizeAngle(angle);
+		return Mathf.Clamp(angle, min, max);
+	}
+	
+	private float NormalizeAngle(float angle)
+	{
+		while (angle > 180f) angle -= 360f;
+		while (angle < -180f) angle += 360f;
+		return angle;
+	}
+	
+	private void OntriggerEnter(Collider other)
+	{
+		// if(other.gameObject.tag == "Finpad")
+		// {
+			// AddReward(2f);
+			// EndEpisode();
+		// }
+		// if(other.gameObject.tag == "Obstacle")
+		// {
+			// AddReward(-1f);
+		// }
+	}
+	
+	public override void Heuristic(in ActionBuffers actionsOut)
+	{
+		ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[6] = Input.GetAxisRaw("Horizontal"); // left and right
+		// continuousActions[1] = Input.GetAxisRaw("Vertical"); // up and down 
+	}
+	
+	void Update()
+	{
+		if (Input.anyKey)
+		{
+			RequestDecision();
+		}
+	}
+}
